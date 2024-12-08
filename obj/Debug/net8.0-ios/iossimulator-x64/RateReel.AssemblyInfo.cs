b301293b8d5a337099868671d4df3a27441c5580
MelvinoRizky <melvinorizkyputrawahyudi@mail.ugm.ac.id>
2024-12-08 16:14:16 +0700

    update it

--- obj/Debug/net8.0-ios/iossimulator-x64/RateReel.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/RateReel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RateReel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+07944b843dc0720ebb0e5bf4be9f55b118afc893")]
 [assembly: System.Reflection.AssemblyProductAttribute("RateReel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RateReel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
